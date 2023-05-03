ae5743a1b8bbd5cd3a462e54dea371d6b2f5c3fa
feipiao594 <445785526@qq.com>
2023-05-03 17:21:15 +0800

    Site updated: 2023-05-03 17:21:14

--- js/category.js
+++ js/category.js
@@ -5,13 +5,14 @@ $(document).ready(function() {
 	function categoryUnfold() {
 		$(".category-list-link").filter(function() {
 			if($(this).siblings(".category-list-child").length > 0)
-				//$(this).parent().css("list-style", "circle");
-				$(this).parent().css("list-style-image","url('/assets/img/category-unfold-16x16.png')");
+				$(this).parent().css("list-style", "circle");
+				//$(this).parent().css("list-style-image","url('/assets/img/category-unfold-16x16.png')");
 				//console.log($(this).text());
 		});
 		
 		//点击含有子标签category-list-child的category-list-link标签时，不进行跳转
 		$(".category-list-link").filter(function() {
+			alert("jQuery is imported");
 			return $(this).siblings(".category-list-child").length > 0
 		}).attr("href", "javascript:void(0)");
 		
